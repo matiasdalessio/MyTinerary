2b1857c603932ec3a152ba960aa042a7669b86d8
matiasdalessio <matidaless@gmail.com>
2023-04-28 15:08:17 -0300

    fix googleID

--- frontend/src/components/GoogleLoginID.js
+++ frontend/src/components/GoogleLoginID.js
@@ -1,3 +1,3 @@
 // export const GoogleLoginID =
 //   "706728189535-iijpb3pdc9oembe5htgf1lfc1cmvbg67.apps.googleusercontent.com";
-export const GoogleLoginID = '706728189535-gkdltcou7njsjagcfhn30q0i25g7f30v.apps.googleusercontent.com'
+export const GoogleLoginID = '706728189535-7mqbutmbjctgobmcqjemuend0urkj1t9.apps.googleusercontent.com'
